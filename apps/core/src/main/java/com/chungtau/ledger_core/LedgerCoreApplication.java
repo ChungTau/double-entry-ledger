9affb8dd1cf47ad44f3de27859fa40a8d4bc12f2
ChungTau <edwardonionc020903@gmail.com>
2025-12-20 04:46:16 +0800

    Update application configuration by adding new Bash commands in settings, enhancing pom.xml with additional dependencies.

--- apps/core/src/main/java/com/chungtau/ledger_core/LedgerCoreApplication.java
+++ apps/core/src/main/java/com/chungtau/ledger_core/LedgerCoreApplication.java
@@ -2,8 +2,10 @@ package com.chungtau.ledger_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.chungtau.ledger_core.repository")
 public class LedgerCoreApplication {
 
 	public static void main(String[] args) {
